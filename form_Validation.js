3306cdb770f567b6f59265b8bf5cec75583e9b58
Nathan <G00423489@atu.ie>
2023-07-28 13:45:40 +0100

    Finalised desktop initial view

--- form_Validation.js
+++ form_Validation.js
@@ -1,27 +1,40 @@
 
-var validation_Error_Message = document.getElementsByClassName("email-error-message");
-const email_Input = document.getElementsByClassName("email-input");
+const form_container  = document.getElementById("form-container");
+const success_container = document.getElementById("success-container");
+const email_input = document.getElementById("email-input");
+const email_submit = document.getElementById("email-submit");
+
+var validEmailRegEx = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
+
 
 
 document.querySelector( "form" ).addEventListener( "invalid", function( event ) 
         {
             event.preventDefault();
-            console.log("submit");
+            console.log("invalid");
 
             document.getElementById("email-error-message").style.display = "block"; 
         }, true );
-
-
-
-document.querySelector( "form" ).addEventListener( "valid", function( event ) 
-{
-    event.preventDefault();
-    window.location.href = "/success.html";
-});
        
-
 function submit_test()
 {
-    
+    form_container.style.display = "none";
+    success_container.style.display = "flex"
+
 }
 
+
+document.querySelector( "form" ).addEventListener( "input", function( event ) 
+        {
+            if(validEmailRegEx.test(email_input.value))
+            {
+                console.log("is valid");
+                email_submit.classList.add("active");
+            }
+            else
+            {
+                email_submit.classList.remove("active");
+            }
+           
+
+        })
